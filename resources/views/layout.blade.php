a69456deba8a59fbf753daf321395391f839a686
kkrolikowski <kkrolikowski.1993@gmail.com>
2024-07-31 20:51:53 +0200

    feat: main layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pet Store App</title>
+</head>
+<body>
+    <img src="https://www.petsmall.pk/wp-content/uploads/2023/08/istockphoto-1445196818-170667a.webp">
+    <a href="/">Home</a>
+    @yield('content')
+    </div>
+</body>
+</html>
